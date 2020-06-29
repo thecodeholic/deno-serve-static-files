b6265587dd5291d0a76d1fbb675175ffeddd8a9b
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-06-29 18:02:14 +0400

    Initial commit

---
+++ oak/server.ts
@@ -0,0 +1,23 @@
+import {Application, Router} from 'https://deno.land/x/oak@v5.3.1/mod.ts'
+import { staticFileMiddleware } from './staticFileMiddleware.ts';
+
+const app = new Application();
+
+const router = new Router();
+router
+  .get('/', (ctx) => {
+    ctx.response.body = 'Home'
+  })
+  .get('/about', (ctx) => {
+    ctx.response.body = 'About'
+  })
+
+app.use(staticFileMiddleware);
+app.use(router.routes());
+app.use(router.allowedMethods())
+
+app.addEventListener('listen', () => {
+  console.log("Server started");
+})
+
+await app.listen({port: 8000})
